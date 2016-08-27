b76c6a50e62a5d6c10d4a540139fbda7f1117c86
laurauzcategui <laura.uzcateguij@gmail.com>
2016-08-28 00:03:22 +0100

    Moving Team Directory to one repo

--- TeamDirectory/Directory/www/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Person
-
-def index(request):
-    people = Person.objects.all()
-    return render(request, 'index.html',{'people':people})
--- TeamDirectory/Procfile
+++
@@ -1,1 +0,0 @@
-web: sh -c "cd Directory && gunicorn Directory.wsgi"
