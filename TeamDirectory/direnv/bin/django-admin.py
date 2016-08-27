843c6e4a14bec4248190d2fc24ad878d29cb949f
laurauzcategui <laura.uzcateguij@gmail.com>
2016-08-27 22:27:00 +0100

    Removing TeamDirectory

--- TeamDirectory/direnv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/laura/Documents/learningPy/django_code/TeamDirectory/direnv/bin/python2
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
